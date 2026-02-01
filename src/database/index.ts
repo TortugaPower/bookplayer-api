663456033167f573a5c31ef7c4550c7f29db5079
Gianni Carlo <gcarlo89@hotmail.com>
2026-02-01 06:40:32 -0500

    fix ssl

--- src/database/index.ts
+++ src/database/index.ts
@@ -9,6 +9,7 @@ export default knex({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    ssl: { rejectUnauthorized: false },
   },
   pool: {
     min: 2,
