fbbd655da760b9e6c878a3a922e42e4ed4ffea1e
Gianni Carlo <gcarlo89@hotmail.com>
2026-02-17 22:16:35 -0500

    fix validation

--- knexfile.ts
+++ knexfile.ts
@@ -11,6 +11,7 @@ module.exports = {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
+      ssl: process.env.DB_HOST === 'localhost' ? false : { rejectUnauthorized: false },
     },
     pool: {
       min: 2,
